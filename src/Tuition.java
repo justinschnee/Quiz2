5e8c8f95ba14ac79844251c7632142dbab5968f5
justinschnee <justinschnee@wifi-roaming-128-4-65-198.host.udel.edu>
2017-02-26 20:24:10 -0500

    quiz2

---
+++ src/Tuition.java
@@ -0,0 +1,34 @@
+import java.util.Scanner; // Utility to accept input from user
+
+public class Tuition {
+	public static void main(String [] args) {
+		
+		
+		Scanner input = new Scanner(System.in);
+
+		//Variables
+		double tuition;
+		double rate;
+		
+		//initial tuition cost
+		System.out.print("What is the initial tuition cost?");
+		tuition = input.nextDouble();
+		
+		
+		// percentage increase
+		System.out.print("What is the percentage increase in tuition each year?");
+		rate = input.nextDouble()/100;
+
+	
+		// for loop
+		double totalcost=0;
+		
+	    for(int i = 0; i <= 4; i++) {
+	    	
+	    	totalcost += (tuition * (Math.pow(1+rate,i)));
+	    }
+		System.out.printf("\nYour total tuition expense is $%.3f", totalcost);		
+		
+	}
+
+}
